df1aa5308135f010e48debfc8db96b06639da259
nokun13 <kennethkim13@gmail.com>
2020-11-22 23:57:52 +0900

    2020-11-22 2nd

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {FaFacebook, FaInstagram, FaYoutube, FaTwitter, FaLinkedin} from 'react-icons/fa';
+import {FaFacebook, FaInstagram, FaTwitter, FaLinkedin, FaGithub} from 'react-icons/fa';
 import { FooterContainer, FooterLink, FooterLinkItems,
     FooterLinksContainer, FooterLinksWrapper, FooterLinkTitle,
     FooterWrap, SocialMedia, SocialMediaWrap,
@@ -43,12 +43,12 @@ const Footer = () => {
                                 <FaInstagram />
                             </SocialIconLink>
                             <SocialIconLink to="/nolink" target="_blank"
-                            aria-label="Youtube">
-                                <FaYoutube />
-                            </SocialIconLink>
-                            <SocialIconLink to="/nolink" target="_blank"
                             aria-label="Twitter">
                                 <FaTwitter />
+                            </SocialIconLink>
+                            <SocialIconLink href="//www.github.com/nokun13" target="_blank"
+                            aria-label="Github">
+                                <FaGithub />
                             </SocialIconLink>
                             <SocialIconLink href="//www.linkedin.com/in/nok-hun-kim-a21068192/" target="_blank"
                             aria-label="Linkedin">
